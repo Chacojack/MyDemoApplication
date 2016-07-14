6e5b73c8b317ff235771eec8ff9e2a36455e3427
zjChai <zejian.chai@foxmail.com>
2016-07-14 22:48:16 +0800

    练习使用ButterKnife 插件

--- app/src/main/java/demo/me/mydemoapplication/plugins/ButterKnifeActivity.java
+++ app/src/main/java/demo/me/mydemoapplication/plugins/ButterKnifeActivity.java
@@ -1,15 +1,20 @@
 package demo.me.mydemoapplication.plugins;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import demo.me.mydemoapplication.R;
 
-public class ButterKniftActivity extends AppCompatActivity {
+public class ButterKnifeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_butter_knift);
     }
+
+
 }
