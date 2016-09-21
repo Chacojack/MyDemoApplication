6d8bb120797b2608d0481a29cb891f5e471707cf
zjChai <zejian.chai@foxmail.com>
2016-09-21 14:23:48 +0800

    add constraint-layout demo

---
+++ app/src/main/java/demo/me/mydemoapplication/ui/ConstraintLayoutActivity.java
@@ -0,0 +1,15 @@
+package demo.me.mydemoapplication.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import demo.me.mydemoapplication.R;
+
+public class ConstraintLayoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_constraint_layout);
+    }
+}
