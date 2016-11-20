6e421f06c9298cd428602c995c57435b8b26858c
Jack Chai <zejian.chai@foxmail.com>
2016-11-20 23:45:52 +0800

    添加了Service的Demo代码

---
+++ app/src/main/java/demo/me/mydemoapplication/service/MyService.java
@@ -0,0 +1,66 @@
+package demo.me.mydemoapplication.service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+/**
+ * Created by zjchai on 2016/11/20.
+ */
+
+public class MyService extends Service {
+
+    private static final String TAG = MyService.class.getSimpleName();
+
+    private MyBinder mMyBinder;
+
+
+    public class MyBinder extends Binder{
+
+        MyService getService(){
+            return MyService.this;
+        }
+
+    }
+
+
+    @Nullable
+    @Override
+    public IBinder onBind(final Intent intent) {
+        Log.d(TAG, "onBind() called with: intent = [" + intent + "]");
+        return mMyBinder = new MyBinder();
+    }
+
+    @Override
+    public void onCreate() {
+        Log.d(TAG, "onCreate() called");
+        super.onCreate();
+    }
+
+    @Override
+    public int onStartCommand(final Intent intent, final int flags, final int startId) {
+        Log.d(TAG, "onStartCommand() called with: intent = [" + intent + "], flags = [" + flags + "], startId = [" + startId + "]");
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.d(TAG, "onDestroy() called");
+        super.onDestroy();
+    }
+
+    @Override
+    public boolean onUnbind(final Intent intent) {
+        Log.d(TAG, "onUnbind() called with: intent = [" + intent + "]");
+        return super.onUnbind(intent);
+    }
+
+    @Override
+    public void onRebind(final Intent intent) {
+        Log.d(TAG, "onRebind() called with: intent = [" + intent + "]");
+        super.onRebind(intent);
+    }
+}
---
+++ app/src/main/java/demo/me/mydemoapplication/service/ServiceActivity.java
@@ -0,0 +1,68 @@
+package demo.me.mydemoapplication.service;
+
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import demo.me.mydemoapplication.R;
+
+public class ServiceActivity extends AppCompatActivity {
+
+    private static final String TAG = ServiceActivity.class.getSimpleName();
+
+    ServiceConnection mServiceConnection;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_service);
+        findViewById(R.id.start).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                startService(new Intent(ServiceActivity.this, MyService.class));
+            }
+        });
+
+        findViewById(R.id.stop).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                stopService(new Intent(ServiceActivity.this, MyService.class));
+            }
+        });
+
+        findViewById(R.id.bind).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                bindService(new Intent(ServiceActivity.this, MyService.class), mServiceConnection = new ServiceConnection() {
+                    @Override
+                    public void onServiceConnected(final ComponentName name, final IBinder service) {
+                        Log.d(TAG, "onServiceConnected() called with: name = [" + name + "], service = [" + service + "]");
+                    }
+
+                    @Override
+                    public void onServiceDisconnected(final ComponentName name) {
+                        Log.d(TAG, "onServiceDisconnected() called with: name = [" + name + "]");
+                    }
+                }, BIND_AUTO_CREATE);
+            }
+        });
+
+        findViewById(R.id.unbind).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                unbindService(mServiceConnection);
+            }
+        });
+        findViewById(R.id.finish).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                finish();
+            }
+        });
+    }
+}
